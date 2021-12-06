0d981545acf0e0653b1a3aa877b93a990da4830a
ArielCristoHaryono <aharyono01@student.ciputra.ac.id>
2021-12-06 09:12:51 +0700

    Add project files.

---
+++ Week13Praktikum/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Week13Praktikum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Berapa Data = ");
+            int data = Convert.ToInt32(Console.ReadLine());
+            Console.Write("NIM              :");
+            int nim = Convert.ToInt32(Console.ReadLine());
+            Console.Write("NAMA             :");
+            int nama = Convert.ToInt32(Console.ReadLine());
+            Console.Write("JENIS KELAMIN    :");
+            int jenisKelamin = Convert.ToInt32(Console.ReadLine());
+            Console.Write("TAHUN MASUK      :");
+            int tahunMasuk = Convert.ToInt32(Console.ReadLine());
+            Console.Write("PROGRAM STUDI    :");
+            int programStudi = Convert.ToInt32(Console.ReadLine());
+            Console.Write("KELAS            :");
+            int kelas = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
